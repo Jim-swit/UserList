ff7e7bb4321f43d2c1bdec20d17a9ea7c3325bc2
jim <jim.jeon@swit.io>
2023-04-13 09:53:54 +0900

    Toolbar 메뉴 삭제
    Toolbar 사용하지 않는 메뉴 삭제 (menu_main.xml)

--- app/src/main/java/org/project/userlist/ui/view/main/MainActivity.kt
+++ app/src/main/java/org/project/userlist/ui/view/main/MainActivity.kt
@@ -3,7 +3,6 @@ package org.project.userlist.ui.view.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
-import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -31,12 +30,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
