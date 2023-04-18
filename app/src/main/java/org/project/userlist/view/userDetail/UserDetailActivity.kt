510223fe0fc08f63f4ee5abf85ec3e21b27e2cfa
jim <jim.jeon@swit.io>
2023-04-18 09:25:15 +0900

    유저 상세 퐈면 이미지 추가
    user_avatar추가

--- app/src/main/java/org/project/userlist/view/userDetail/UserDetailActivity.kt
+++ app/src/main/java/org/project/userlist/view/userDetail/UserDetailActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.project.userlist.R
 import org.project.userlist.data.network.ApiResult
 import org.project.userlist.databinding.ActivityUserDetailBinding
 import org.project.userlist.model.User
@@ -16,39 +18,43 @@ class UserDetailActivity : ViewBindingBaseActivity<ActivityUserDetailBinding>
     ({ActivityUserDetailBinding.inflate(it)}){
 
     private val userDetailViewModel: UserDetailViewModel by viewModel<UserDetailViewModel>()
+    private val progressDialog by lazy { ProgressDialog(this@UserDetailActivity) }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val dialog = ProgressDialog(this@UserDetailActivity)
 
         lifecycleScope.launch {
             userDetailViewModel.getUser.observe(this@UserDetailActivity, Observer { result ->
                 when (result) {
                     is ApiResult.Success -> {
                         if(result.data is User) {
-                            binding.userLogin.text = result.data.login
-                            dialog.dismiss()
+                            setContent(result.data)
+                            progressDialog.dismiss()
                             showView()
                         }
                     }
 
                     is ApiResult.Loading -> {
                         hideView()
-                        dialog.show()
+                        progressDialog.show()
                     }
 
                     is ApiResult.Fail.Error -> {
-                        dialog.dismiss()
+                        progressDialog.dismiss()
                     }
 
                     is ApiResult.Fail.Exception -> {
-                        dialog.dismiss()
+                        progressDialog.dismiss()
                     }
                 }
             })
         }
 
+        getUser()
+    }
+    private fun getUser() {
         intent?.extras?.getString("user_login").let { login ->
             if(!login.isNullOrEmpty()) {
                 lifecycleScope.launch(Dispatchers.IO) {
@@ -57,11 +63,26 @@ class UserDetailActivity : ViewBindingBaseActivity<ActivityUserDetailBinding>
             }
         }
     }
+
+    private fun setContent(data: User) {
+        binding.userLogin.text = data.login
+
+        binding.userAvatar.clipToOutline = true
+
+        Glide.with(this@UserDetailActivity)
+            .load(data.avatar_url)
+            .placeholder(R.drawable.user_default)
+            .error(R.drawable.user_default)
+            .into(binding.userAvatar)
+    }
+
     private fun hideView() {
         binding.userLogin.visibility = View.GONE
+        binding.userAvatar.visibility = View.GONE
     }
 
     private fun showView() {
         binding.userLogin.visibility = View.VISIBLE
+        binding.userAvatar.visibility = View.VISIBLE
     }
 }
